7251920b27dfc2a69ed5716d6b6029e01549d8c2
nawfalel1 <elachar2001@gmail.com>
2024-01-14 22:49:04 +0100

    Part1 done

--- src/main/java/com/elacharnawfal/demospringkafka/services/PageEventService.java
+++ src/main/java/com/elacharnawfal/demospringkafka/services/PageEventService.java
@@ -1,2 +1,57 @@
-package com.elacharnawfal.demospringkafka.services;public class PageEventService {
+package com.elacharnawfal.demospringkafka.services;
+
+import com.elacharnawfal.demospringkafka.entities.PageEvent;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.autoconfigure.pulsar.PulsarProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Component
+public class PageEventService {
+
+
+
+//    @KafkaListener(topics = "R4",groupId = "com.elacharnawfal")
+//    public void consume(PageEvent pageEvent){
+//
+//            System.out.println("******------************");
+//            System.out.println(pageEvent.toString());
+//            System.out.println("*******-----**********");
+//    }
+
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return (pageEvent -> {
+            System.out.println("******------************");
+            System.out.println(pageEvent.toString());
+            System.out.println("*******-----**********");
+        });
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->
+          new PageEvent(
+                  Math.random()>0.5 ? "P1" : "P2",
+                  Math.random()>0.5 ? "U1" : "U2",
+                  new Date(),
+                  new Random().nextLong(9000)
+          );
+        }
+
+    @Bean
+    public Function<PageEvent,PageEvent> pageEventFunction(){
+        return (input)->{
+            input.setName("PageEvent");
+            input.setUser("Nawfal");
+            return input;
+        };
+    }
 }
--- src/main/java/com/elacharnawfal/demospringkafka/web/PageEventController.java
+++ src/main/java/com/elacharnawfal/demospringkafka/web/PageEventController.java
@@ -16,10 +16,13 @@ import java.util.Random;
 public class PageEventController {
     private final StreamBridge streamBridge;
 
-    @GetMapping("/publish/{topic}/{name}")
-    public PageEvent publish(@PathVariable String topic , @PathVariable String name){
-
-        PageEvent pageEvent = new PageEvent(name,Math.random()>0.5?"U1":"U2",new Date(),new Random().nextLong(9000));
+    @GetMapping("publish/{topic}/{name}")
+    public PageEvent publish(@PathVariable String name, @PathVariable String topic){
+        PageEvent pageEvent=new PageEvent();
+        pageEvent.setName(name);
+        pageEvent.setDate(new Date());
+        pageEvent.setDuration((long) new Random().nextInt(1000));
+        pageEvent.setUser(Math.random()>0.5?"U1":"U2");
         streamBridge.send(topic,pageEvent);
         return pageEvent;
     }
